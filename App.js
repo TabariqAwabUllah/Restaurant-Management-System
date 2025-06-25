5d00a17d9f99f820e74dc07de3aafe70f0d3535f
Tabariqawabullah <Tabariqawabullah@gmail.com>
2025-06-25 18:06:19 +0500

    Connecting Firebase

---
+++ App.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>ok</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
